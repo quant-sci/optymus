16374fe1a1ba8501ff472ce47a4ca90066d09cf7
kleyt0n <kleyton.vsc@gmail.com>
2024-06-19 00:51:01 -0300

    fix: dependencies erros in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,12 +14,9 @@ import sys
 import os
 import shutil
 
-sys.path.insert(0, os.path.abspath(".."))
-work_dir = '/'.join(os.getcwd().split("/")[:-2])
-src_path = os.path.join(work_dir,'src')
-print(src_path)
-
-sys.path.insert(0, src_path)
+	
+sys.path.insert(0, os.path.abspath('../'))
+print(sys.path)
 
 # notebooks_path = os.path.join(work_dir, 'notebooks')
 
