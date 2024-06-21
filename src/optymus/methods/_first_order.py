fix: gradient descent -> steepest descent

src/optymus/methods/_first_order.py
@@ -7,10 +7,11 @@ from tqdm import tqdm
 from optymus.search import line_search
 
 
-def gradient_descent(f_obj=None, f_cons=None, args=(), args_cons=(), x0=None, tol=1e-4, learning_rate=0.01, max_iter=100, verbose=True, maximize=False):
-    r"""Gradient Descent
+def steepest_descent(f_obj=None, f_cons=None, args=(), args_cons=(), x0=None, tol=1e-4, learning_rate=0.01,
+                     max_iter=100, verbose=True, maximize=False):  # noqa: FBT002
+    r"""Steepest Descent
 
-    Gradient Descent is a first-order optimization algorithm that uses the
+    Steepest Descent is a first-order optimization algorithm that uses the
     gradient of the objective function to compute the step direction.
 
     We can minimize the objective function :math:`f` by solving the following
@@ -77,13 +78,13 @@ def gradient_descent(f_obj=None, f_cons=None, args=(), args_cons=(), x0=None, to
     alphas = []
     num_iter = 0
 
-    progres_bar = tqdm(range(max_iter), desc=f'Gradient Descent {num_iter}',) if verbose else range(max_iter)
+    progres_bar = tqdm(range(max_iter), desc=f'Steepest Descent {num_iter}',) if verbose else range(max_iter)
 
     for _ in progres_bar:
         if jnp.linalg.norm(grad) < tol:
             break
         r = line_search(f=penalized_obj, x=x, d=d, learning_rate=learning_rate)
-        x = r['xopt'].astype(float)  # Ensure xopt is of a floating-point type
+        x = r['xopt'].astype(float)
         grad = jax.grad(penalized_obj)(x)
         d = grad
         path.append(x)
@@ -94,7 +95,7 @@ def gradient_descent(f_obj=None, f_cons=None, args=(), args_cons=(), x0=None, to
     elapsed_time = end_time - start_time
 
     return {
-        'method_name': 'Gradient Descent' if not f_cons else 'Gradient Descent with Penalty',
+        'method_name': 'Steepest Descent' if not f_cons else 'Steepest Descent with Penalty',
         'x0':x0,
         'xopt': x,
         'fmin': f_obj(x, *args),
@@ -105,7 +106,8 @@ def gradient_descent(f_obj=None, f_cons=None, args=(), args_cons=(), x0=None, to
     }
 
 
-def conjugate_gradient(f_obj=None, f_cons=None, args=(), args_cons=(), x0=None, tol=1e-5, learning_rate=0.01, max_iter=100, verbose=True, gradient_type='fletcher_reeves', maximize=False):
+def conjugate_gradient(f_obj=None, f_cons=None, args=(), args_cons=(), x0=None, tol=1e-5, learning_rate=0.01,
+                       max_iter=100, verbose=True, gradient_type='fletcher_reeves', maximize=False):  # noqa: FBT002
     r"""Conjugate Gradient
 
     Conjugate Gradient is a first-order optimization algorithm that uses the
@@ -132,7 +134,7 @@ def conjugate_gradient(f_obj=None, f_cons=None, args=(), args_cons=(), x0=None, 
 
     - Fletcher-Reeves: :math:`\beta_k = \frac{\nabla x_k^{T} \nabla x_k}{\nabla x_{k-1}^{T} \nabla x_{k-1}}`
     - Polak-Ribiere: :math:`\beta_k = \frac{\nabla x_k^{T} (\nabla x_k - \nabla x_{k-1})}{\nabla x_{k-1}^T \nabla x_{k-1}}`
-    - Hestnes-Stiefel: :math:`\beta_k = \frac{\nabla x_k^{T} (\nabla x_k - \nabla x_{k-1})}{\nabla s_{k-1}^{T}(\nabla x_{k} - \nabla x_{k-1})}` 
+    - Hestnes-Stiefel: :math:`\beta_k = \frac{\nabla x_k^{T} (\nabla x_k - \nabla x_{k-1})}{\nabla s_{k-1}^{T}(\nabla x_{k} - \nabla x_{k-1})}`
     - Dai-Yuan: :math:`\beta_k = \frac{\nabla x_{k}^{T} \nabla x_{k}}{\nabla s_{k-1}^{T}(\nabla x_{k} - \nabla x_{k-1})}`
 
     Parameters
@@ -224,7 +226,8 @@ def conjugate_gradient(f_obj=None, f_cons=None, args=(), args_cons=(), x0=None, 
     elapsed_time = end_time - start_time
 
     return {
-        'method_name': f'Conjugate Gradients ({gradient_type})' if not f_cons else f'Conjugate Gradients ({gradient_type}) with Penalty',
+        'method_name': f'Conjugate Gradients ({gradient_type})' if not f_cons else \
+            f'Conjugate Gradients ({gradient_type}) with Penalty',
         'x0':x0,
         'xopt': x,
         'fmin': f_obj(x, *args),
@@ -235,7 +238,8 @@ def conjugate_gradient(f_obj=None, f_cons=None, args=(), args_cons=(), x0=None, 
         }
 
 
-def bfgs(f_obj=None, f_cons=None, args=(), args_cons=(), x0=None, tol=1e-5, learning_rate=0.01, max_iter=100, verbose=True, maximize=False):
+def bfgs(f_obj=None, f_cons=None, args=(), args_cons=(), x0=None, tol=1e-5, learning_rate=0.01, max_iter=100,
+         verbose=True, maximize=False):  # noqa: FBT002
     r"""BFGS
 
     BFGS is a first-order optimization algorithm that uses the gradient of the

src/optymus/optimizer/_optimizer.py
@@ -1,7 +1,7 @@
+import dash  # noqa: INP001
+import dash_bootstrap_components as dbc
 import jax
 import pandas as pd
-import dash
-import dash_bootstrap_components as dbc
 from dash import Input, Output, dcc, html
 from dash_bootstrap_templates import ThemeSwitchAIO, load_figure_template
 from optymus.methods import (
@@ -10,10 +10,10 @@ from optymus.methods import (
     adamax,
     bfgs,
     conjugate_gradient,
-    gradient_descent,
     newton_raphson,
     powell,
     rmsprop,
+    steepest_descent,
     univariant,
     yogi,
 )
@@ -24,7 +24,7 @@ jax.config.update("jax_enable_x64", True)
 METHODS = {
     "univariant": univariant,
     "powell": powell,
-    "gradient_descent": gradient_descent,
+    "steepest_descent": steepest_descent,
     "conjugate_gradient": conjugate_gradient,
     "bfgs": bfgs,
     "newton_raphson": newton_raphson,
@@ -37,14 +37,14 @@ METHODS = {
 
 
 class Optimizer:
-    def __init__(self, f_obj=None, f_cons=None, x0=None, method='gradient_descent', **kwargs):
+    def __init__(self, f_obj=None, f_cons=None, x0=None, method='steepest_descent', **kwargs):
         """
         Initializes the Optimizer class.
 
         Args:
             f_obj (function): The objective function to be minimized.
             x0 (np.ndarray): The initial guess for the minimum.
-            method (str, optional): The optimization method to use. Defaults to 'gradient_descent'.
+            method (str, optional): The optimization method to use. Defaults to 'steepest_descent'.
         """
         self.f_obj = f_obj
         self.f_cons = f_cons
@@ -158,8 +158,8 @@ class Optimizer:
                                         html.Tr([html.Th("Parameter"), html.Th("Value")]),
                                         html.Tr([html.Td("Method"), html.Td(str(self.method))]),
                                         html.Tr([html.Td("Final Solution"), html.Td(str(self.opt.get('xopt', 'N/A')))]),
-                                        html.Tr([html.Td("Objective Function Value"), html.Td(str(self.opt.get('fmin', 'N/A')))]),
-                                        html.Tr([html.Td("Number of Iterations"), html.Td(str(self.opt.get('num_iter', 'N/A')))]),
+                                        html.Tr([html.Td("Objective Function Value"), html.Td(str(self.opt.get('fmin', 'N/A')))]),  # noqa: E501
+                                        html.Tr([html.Td("Number of Iterations"), html.Td(str(self.opt.get('num_iter', 'N/A')))]),  # noqa: E501
                                         html.Tr([html.Td("Initial Guess"), html.Td(str(self.x0))]),
                                         html.Tr([html.Td("Time Elapsed"), html.Td(str(self.opt.get('time', 'N/A')))]),
                                     ],
@@ -203,7 +203,7 @@ class Optimizer:
             theme = "flatly" if toggle else "slate"
             load_figure_template(theme)
             return plot_optim(f_obj=self.f_obj, f_cons=self.f_cons, x0=self.x0, method=self.opt, path=True, template=theme, **kwargs), \
-                   plot_alphas(self.opt.get('alphas', None), template=theme)
+                   plot_alphas(self.opt.get('alphas', None), template=theme)  # noqa: E501
 
 
         # Run the Dash app

tests/test_methods.py
@@ -1,7 +1,7 @@
 import pytest
 import jax.numpy as jnp
 from optymus.methods import (
-    gradient_descent, 
+    steepest_descent, 
     conjugate_gradient, 
     bfgs, 
     newton_raphson, 
@@ -21,8 +21,8 @@ tol = 1e-5
 learning_rate = 0.1
 max_iter = 1000
 
-def test_gradient_descent():
-    result = gradient_descent(f_obj=f_obj, x0=x0, tol=tol,
+def test_steepest_descent():
+    result = steepest_descent(f_obj=f_obj, x0=x0, tol=tol,
                               learning_rate=learning_rate, max_iter=max_iter, verbose=False)
     assert jnp.linalg.norm(result['xopt']) < tol
     assert result['num_iter'] <= max_iter