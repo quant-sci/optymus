ce946410fc38281ab8c812d6acd00d0034a80b9f
kleyt0n <kleyton.vsc@gmail.com>
2025-12-23 02:30:18 +0000

    feat: include LBFGS method in the optimizer module

--- src/optymus/methods/__init__.py
+++ src/optymus/methods/__init__.py
@@ -1,5 +1,5 @@
 from optymus.methods.adaptive import adagrad, adam, adamax, rmsprop, yogi
-from optymus.methods.first_order import bfgs, conjugate_gradient, steepest_descent
+from optymus.methods.first_order import bfgs, conjugate_gradient, lbfgs, steepest_descent
 from optymus.methods.population import differential_evolution, particle_swarm
 from optymus.methods.second_order import newton_raphson
 from optymus.methods.stochastic import cmaes, cross_entropy, simulated_annealing
@@ -12,6 +12,7 @@ __all__ = [
     "steepest_descent",
     "conjugate_gradient",
     "bfgs",
+    "lbfgs",
     "newton_raphson",
     "adagrad",
     "rmsprop",
